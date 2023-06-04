1a12587d507098362b0191f4f7514c384a6c628c
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2023-06-04 13:24:55 +0300

    Completed the Hero Section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,7 +25,7 @@ const Navbar = () => {
 
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
-      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+      <div className="w-full flex justify-between items-center mx-auto">
         <Link to="/" className="flex items-center gap-2" onClick={handleLogoClick}>
           <img src={logo} alt="logo" className="w-16 h-16 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer">
